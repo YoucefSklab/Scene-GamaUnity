9c931835301df167b6e21f0e6318af392c0611c2
SKLAB <ysklab@hotmail.com>
2018-07-10 11:41:42 +0200

    Mqtt script
    Add Mqtt Script to manage Messages, Exceptions, Utility and tests

---
+++ GamaUnity/Assets/Script MQTT/test/mqttTest.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt.Utility;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+
+using System;
+
+public class mqttTest : MonoBehaviour {
+	private MqttClient client;
+	// Use this for initialization
+	void Start () {
+		// create client instance 
+		//client = new MqttClient(IPAddress.Parse("143.185.118.233"),8080 , false , null ); 
+		client = new MqttClient("localhost" ); 
+
+		// register to message received 
+		client.MqttMsgPublishReceived += client_MqttMsgPublishReceived; 
+		
+		string clientId = Guid.NewGuid().ToString(); 
+		client.Connect(clientId); 
+		
+		// subscribe to the topic "/home/temperature" with QoS 2 
+		client.Subscribe(new string[] { "hello/world" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE }); 
+
+	}
+	void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) 
+	{ 
+		string receivedMessage = System.Text.Encoding.UTF8.GetString (e.Message);
+		Debug.Log("Received: " +  receivedMessage );
+		Debug.Log("Good... Done!");
+		
+
+	} 
+
+	void OnGUI(){
+		if ( GUI.Button (new Rect (20,40,180,20), "Level 1 ... Cliquez ici")) {
+			Debug.Log("sending... SKLAB");
+			client.Publish("hello/world", System.Text.Encoding.UTF8.GetBytes("Sending from Unity3D!!! Good"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+			Debug.Log("sent");
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+}
